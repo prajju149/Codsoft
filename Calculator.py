03fa668dff561c0cb546b8c64e7de9c8f2f874e3
prajju149 <aksir648@gmail.com>
2024-08-31 01:27:43 +0530

    Add files via upload
    A simple calculator with basic arithmetic operations. Prompt the user to input two numbers and an operation choice. Perform the calculation and display the result.

---
+++ Calculator.py
@@ -0,0 +1,20 @@
+print("****** Simple Calculator ******")
+
+num1 = float(input("Enter first number : "))
+num2 = float(input("Enter second number : "))
+
+print("press 1 for Addition \npress 2 for Subtraction \npress 3 for multiplication \npress 4 for Division")
+
+choice = int(input(" Enter your choice from 1-4 : "))
+
+if choice == 1:
+    print(num1 + num2)    
+elif choice == 2:
+    print(num1 - num2)    
+elif choice == 3:
+    print(num1 * num2)    
+elif choice == 4:
+    print(num1 / num2)
+else:
+    print("Invalid Number")
+    
